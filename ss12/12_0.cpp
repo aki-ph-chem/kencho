828ca807015c18ea23d78698500f035e00ce32a0
aki <aki@mymail.com>
2022-12-22 09:09:33 +0900

    挿入ソート

---
+++ ss12/12_0.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+
+void I_Sort(vector<int> &a){
+    int N = (int)a.size();
+    for(int i = 1; i < N ;i++){
+        // 挿入する値v
+        int v = a[i];
+        // vを挿入する場所j
+        int j = i;
+        for(; j > 0; --j){
+            if( a[j-1] > v){
+                a[j] = a[j-1];
+            }
+            else break;
+        }
+        a[j] = v;
+    }
+}
+
+int main(){
+    int N;
+    std::cin >> N;
+    std::vector<int> a(N);
+
+    for(int i = 0; i < N ;i++) std::cin >> a[i];
+
+    I_Sort(a);
+    for(int k = 0; k < N ; k++){
+        std::cout << a[k] ;
+    }
+
+    std::cout << std::endl;
+}
