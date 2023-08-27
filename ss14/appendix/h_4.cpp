3fc04ded9ebe60cbd5db5df7a177f5c4eccc49c1
aki <aki@mymail.com>
2023-08-28 01:53:02 +0900

    fix

--- ss14/appendix/h_4.cpp
+++ ss14/appendix/h_4.cpp
@@ -5,45 +5,45 @@
 #include <queue>
 #include <string>
 
-struct Permission {
-    int read;
-    int write;
-    int exec;
+struct Coordinate3D {
+    int x;
+    int y;
+    int z;
 
-    Permission(int read, int write, int exec)
-        : read(read), write(write), exec(exec) 
+    Coordinate3D(int x, int y, int z)
+        : x(x), y(y), z(z) 
     {}
 
     // greater
-    bool operator > (const Permission& other) const {
-        return read < other.read;
+    bool operator > (const Coordinate3D& other) const {
+        return x < other.x;
     }
 
     // less
-    bool operator < (const Permission& other) const {
-        return read > other.read;
+    bool operator < (const Coordinate3D& other) const {
+        return x > other.x;
     }
 };
 
 // greaterに変更
 using QG = std::priority_queue<
-    Permission,
-    std::vector<Permission>,
-    std::greater<Permission>>;
+    Coordinate3D,
+    std::vector<Coordinate3D>,
+    std::greater<Coordinate3D>>;
 
 int main(void) {
     auto array = std::array<long long,6>{1, 2, 3, 4, 5, 6}; 
     QG qg_0;
 
     for(std::size_t i = 0; i < array.size(); ++i){
-        qg_0.push(Permission(array[i], 2 *(-array[i]), 2 * array[i] + 1));
+        qg_0.push(Coordinate3D(array[i], 2 *(-array[i]), 2 * array[i] + 1));
     }
 
     std::cout << "std::greater" << "\n";
     while(!qg_0.empty()) {
         auto v = qg_0.top();
-        std::cout << "v: read = " << v.read << " v: write = " << v.write;
-        std::cout << " v: exec = " << v.exec << std::endl;
+        std::cout << "v: x = " << v.x << " v: y = " << v.y;
+        std::cout << " v: z = " << v.z << std::endl;
         qg_0.pop();
     }
 }
